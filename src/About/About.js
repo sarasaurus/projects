d56d2f2bd02da731262dfe01fb18203e9d3969d1
sarasaurus <bixler.s.e@gmail.com>
2018-10-03 10:44:29 -0700

    about scaffold

---
+++ src/About/About.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './About.css';
+
+class About extends Component {
+  render() {
+    return (
+      <div className="About">
+   
+        <p className="About-intro">
+        Suup this is Sarah
+        </p>
+      </div>
+    );
+  }
+}
+
+export default About;
